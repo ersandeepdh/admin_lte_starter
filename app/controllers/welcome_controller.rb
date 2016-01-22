2b16cfea19c60d9b221a99c04f2971bfe353822b
Anil Kumar <aniltimt@gmail.com>
2016-01-22 12:51:07 +0530

    login-signup page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def login
+  	render :layout => false
+  end
+
+  def signup
+  	render :layout => false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
+   get '/login' => 'welcome#login'
+   get '/signup' => 'welcome#signup'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
